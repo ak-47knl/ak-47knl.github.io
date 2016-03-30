f8c7b34b4f5ce8df76f027cae7fb9ad3f4ad62a1
Arsen Bespalov <me@arsen.pw>
2016-03-30 10:36:23 +0800

    update site: change contact form

--- assets/js/main.js
+++ assets/js/main.js
@@ -131,7 +131,6 @@
 
 					// Everything look good! proceed...
 					if (proceed) {
-						console.log('Yo! Proceed...');
 						// Data to be send to server
 						var _data = {
 							'userName': _name,
@@ -146,6 +145,7 @@
 							crossDomain: false,
 							dataType: 'json',
 							success: function(response) {
+								console.log('Yo! Proceed...');
 								if (response.type == 'error') {
 									console.error(response.text);
 								} else {
